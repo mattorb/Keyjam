3ad8a7a2d210f343d0734353711601de550b3879
mattorb <matt@piikeastreet.com>
2025-02-21 13:17:31 -0600

    apply formatting per lint rules and editorconfig

--- Keyjam/Keyjam/KeyjamApp.swift
+++ Keyjam/Keyjam/KeyjamApp.swift
@@ -2,9 +2,9 @@ import SwiftUI
 
 @main
 struct KeyjamApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
